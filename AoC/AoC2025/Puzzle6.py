51f04af45779b6c3dde8f65696580e7ca10d7817
Freerk de Leeuw <freerk.de.leeuw@bd.com>
2025-12-18 15:50:02 +0100

    Stepje further

--- AoC/AoC2025/Puzzle6.py
+++ AoC/AoC2025/Puzzle6.py
@@ -7,7 +7,7 @@ class Puzzle6(PuzzleBase):
         return """123 328  51 64 
  45 64  387 23 
   6 98  215 314
-*   +   *   + """
+*   +   *   +  """
 
     def solve_part1(self) -> str:
         lines = self.get_puzzle_input().strip().splitlines()
@@ -34,16 +34,26 @@ class Puzzle6(PuzzleBase):
         return answer
 
     def solve_part2(self) -> str:
-        lines = self.get_puzzle_input().strip().splitlines()
-        numbers_regex = re.compile(r'\d+')
-        operators_regex = re.compile(r'[\+\*]')
-        numbers = []
-        answer = 0;
-        for line in lines[0:-1]:
-            numbers.append(numbers_regex.findall(line))
-        
-        operators = operators_regex.findall(lines[-1])
+        lines = self.get_puzzle_input().splitlines()
 
-        return "hellup"
-        #for problem in range(len(numbers[0])):
+        # operator is the startindex
+        start = 0
+        operator_line = lines[-1]
+        operator_line_len = len(operator_line)
+        for index in range(operator_line_len):
+            if index == operator_line_len-1 or operator_line[index+1] in ['+', '*']:
+                if (index == operator_line_len-1):
+                    index += 1
+
+                matrix = [[c for c in line[start:index]] for line in lines[0:-1]]
+
+                # Transpose using zip
+                transposed = [list(row) for row in zip(*matrix)]
+                print(transposed)
+                start = index+1
+
+
+
+
+        return "lekker busy"
 
